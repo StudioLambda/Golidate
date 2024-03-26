32d56fdfbb285ca45d176c728e974ab8121bd26c
Èrik C. Forés <soc@erik.cat>
2024-03-26 08:20:20 +0100

    feat: fix typings for maps

--- rule/map_keys.go
+++ rule/map_keys.go
@@ -6,7 +6,7 @@ import (
 	"github.com/studiolambda/golidate"
 )
 
-func MapKeys[M map[K]V, K comparable, V any](rules ...golidate.Rule) golidate.Rule {
+func MapKeys[M ~map[K]V, K comparable, V any](rules ...golidate.Rule) golidate.Rule {
 	return func(value any) golidate.Result {
 		result := golidate.
 			Uncertain(value, "map_keys")
--- rule/map_values.go
+++ rule/map_values.go
@@ -6,7 +6,7 @@ import (
 	"github.com/studiolambda/golidate"
 )
 
-func MapValues[M map[K]V, K comparable, V any](rules ...golidate.Rule) golidate.Rule {
+func MapValues[M ~map[K]V, K comparable, V any](rules ...golidate.Rule) golidate.Rule {
 	return func(value any) golidate.Result {
 		result := golidate.
 			Uncertain(value, "map_values")
