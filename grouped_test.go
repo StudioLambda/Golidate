866ed5e5819e64bbaf353bd7807eef8bc57d58d7
Erik C. Forés <soc@erik.cat>
2026-05-19 12:46:20 +0200

    test: cover grouped messages

--- grouped_test.go
+++ grouped_test.go
@@ -1,7 +1,27 @@
 package golidate_test
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"github.com/studiolambda/golidate"
+	"github.com/studiolambda/golidate/format"
+)
 
 func TestGroupedMessages(t *testing.T) {
-	//
+	grouped := golidate.Grouped{
+		"name": golidate.Results{
+			golidate.Fail("", "required").Name("name").WithMetadata(golidate.Metadata{}),
+		},
+		"email": golidate.Results{
+			golidate.Fail("invalid", "email").Name("email").WithMetadata(golidate.Metadata{}),
+		},
+	}
+	grouped["name"][0].Message = "name is required"
+	grouped["email"][0].Message = "email is invalid"
+
+	messages := grouped.Messages(format.Capitalize(), format.Punctuate())
+
+	require.Equal(t, []string{"Name is required."}, messages["name"])
+	require.Equal(t, []string{"Email is invalid."}, messages["email"])
 }
